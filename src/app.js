2bafbc5a79b778dc4e327e82b9c883e7de38a1cf
Gabriel <gabrielproencaaraujo@gmail.com>
2022-11-04 17:25:09 -0300

    Item 3

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { readTalker } = require('./utils/fsUtils');
+const { generateToken } = require('./utils/functionUtils');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const NOT_FOUND = 404;
+const UNAUTHORIZED = 401;
+/* const CREATED = 201; */
+const OK = 200;
 
 app.get('/talker', async (req, res) => {
     const talker = await readTalker();
@@ -25,6 +29,16 @@ app.get('/talker/:id', async (req, res) => {
         return res.status(NOT_FOUND).json({ message: 'Pessoa palestrante não encontrada' });
     } 
     return res.status(HTTP_OK_STATUS).json(response);
+});
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    if ([email, password].includes(undefined)) {
+        return res.status(UNAUTHORIZED).json({ message: 'Campos ausentes!' });
+    }
+    const token = generateToken();
+    return res.status(OK).json({ token });
 });
 
 module.exports = app;
