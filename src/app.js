d663ac4438fe1d9d0956c4feea14aee51072f607
Gabriel <gabrielproencaaraujo@gmail.com>
2022-11-04 16:30:35 -0300

    item 2

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const NOT_FOUND = 404;
 
 app.get('/talker', async (req, res) => {
     const talker = await readTalker();
@@ -13,6 +14,17 @@ app.get('/talker', async (req, res) => {
         return res.status(HTTP_OK_STATUS).json([]);
     } 
     return res.status(HTTP_OK_STATUS).json(talker);
+});
+
+app.get('/talker/:id', async (req, res) => {
+    const { id } = req.params;
+    const talker = await readTalker();
+    const response = talker.find((t) => t.id === Number(id));
+
+    if (!response) {
+        return res.status(NOT_FOUND).json({ message: 'Pessoa palestrante não encontrada' });
+    } 
+    return res.status(HTTP_OK_STATUS).json(response);
 });
 
 module.exports = app;
